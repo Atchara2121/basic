54121bb56016ca346bdeac2f16fc7ba1fedfac55
Your Name <you@example.com>
2024-06-24 15:32:07 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,29 @@
+export default function Home() {
+  return (
+    <>
+   <div className="container-fluid">
+
+      <div className="row">
+          <div className="col-md-12 text-center  bg-primary p-5">
+              HEADER1
+        </div>
+      </div>
+     
+      <div className="row">
+          <div className="col-md-6 text-center  bg-warning p-5">
+             COLUMN1
+       </div>
+       <div className="col-md-6 text-center  bg-success p-5">
+             COLUMN2
+      </div>
+   </div>
+   <div className="row">
+          <div className="col-md-12 text-center  bg-primary p-5">
+              Footer
+              </div>
+              </div>
+  </div>
+    </>
+  );
+}
+
